eb27dc659be6627de12119e1d4173162f31446ac
gumper-x <gumper2002@gmail.com>
2023-03-29 18:48:50 +0300

    feat: add footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,7 +8,7 @@
     padding: 0.8rem 1.4rem;
     margin-bottom: 1rem;
     color: rgb(238, 244, 246);
-    background-color: rgb(33, 71, 151);
+    background-color: $primary;
     transition: box-shadow 0.3s ease-in-out;
     &:hover {
       box-shadow: 0 3px 10px rgba(6, 7, 41, 0.3);
