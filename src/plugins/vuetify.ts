016e6fff8950b4687e124548ba47f4b144f50aed
gumper-x <gumper2002@gmail.com>
2023-03-29 18:47:51 +0300

    feat: add icons

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,12 +3,21 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import { en } from "vuetify/locale";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+import "@mdi/font/css/materialdesignicons.css";
 
 export default createVuetify({
   locale: {
     locale: "en",
     fallback: "en",
     messages: { en },
+  },
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
   },
   theme: {
     defaultTheme: "light",
